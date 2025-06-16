325307fd002241d3c9110f52ef5bdf82e9fff9fe
PAVEL <113603127+Pavlushechko@users.noreply.github.com>
2025-06-16 12:09:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: { 
+    port: 3000 ,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000/",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 })
